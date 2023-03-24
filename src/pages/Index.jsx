57db2f46fc5fed15e6e76a874d1353355cdbb0fe
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-03-24 19:46:44 -0300

    fix routing

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
-import { GiAirplaneDeparture } from "react-icons/gi";
 import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
 
 export const Index = () => {
     return (
@@ -9,18 +9,20 @@ export const Index = () => {
                     <h1 className="text-5xl mb-5 font-['Lobster']">Welcome to your Travel Wishlist</h1>
                     <hr className="border border-pink-500 mb-4 mt-2" />
                     <motion.p
-                         animate={{ x: [1, -100, 1, 100, 1] }}
-                         transition={{ repeat:Infinity, repeatType: "loop", duration: 10  }}
+                        animate={{ x: [1, -100, 1, 100, 1] }}
+                        transition={{ repeat: Infinity, repeatType: "loop", duration: 10 }}
 
                         className="text-center mt-5 text-4xl font-['Amatic_SC']">The place where your dreams begin</motion.p>
                 </div>
                 <div className="grid place-items-center">
-                    <motion.a
-                        whileHover={{ scale: 1.2 }}
-                        transition={{ type: "spring", stiffness: 50 }}
-                        className="bg-pink-500 p-3 shadow-lg shadow-pink-500/50 hover:bg-pink-600 hover:shadow-pink-600/50 transition-colors rounded flex" href="/start">
-                        <span className="text-xl text-xl font-['Cutive_Mono']">Take off!</span>
-                    </motion.a>
+                    <NavLink to="/start">
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            transition={{ type: "spring", stiffness: 50 }}
+                            className="bg-pink-500 p-3 shadow-lg shadow-pink-500/50 hover:bg-pink-600 hover:shadow-pink-600/50 transition-colors rounded flex">
+                            <span className="text-xl text-xl font-['Cutive_Mono']">Take off!</span>
+                        </motion.div>
+                    </NavLink>
                 </div>
             </div>
         </div>
