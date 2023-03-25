5e33906f71b70a4617ccacbe189881f9096ca7ff
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-03-25 12:53:33 -0300

    fix explore form placeholder

--- src/components/ExploreForm.jsx
+++ src/components/ExploreForm.jsx
@@ -1,6 +1,8 @@
+import { useRef } from 'react';
 import { toast } from 'react-toastify';
 
 export const ExploreForm = ({ handleSearch }) => {
+    const inputRef = useRef(null);
     const search = async (e) => {
         e.preventDefault()
         const option = e.target.searchOptions;
@@ -13,7 +15,26 @@ export const ExploreForm = ({ handleSearch }) => {
             toast.error("Oops! Could not find that one.")
         }
     }
-
+    const changePlaceHolder = (e) => {
+        console.log(e)
+        switch(e.target.value){
+            case "name":
+                inputRef.current.placeholder = "e.g. France";
+            break;
+            case "capital":
+                inputRef.current.placeholder="e.g. Paris";
+            break;
+            case "region": 
+                inputRef.current.placeholder="e.g. Europe";
+            break;
+            case "subregion":
+                inputRef.current.placeholder="e.g. Western Europe";
+            break;
+            default:
+                inputRef.current.placeholder="";
+            break;
+        }
+    }
     return (
         <div className="m-3 p-5 mt-0 pt-1">
             <form action="" className="mt-2" onSubmit={(e) => search(e)}>
@@ -22,14 +43,14 @@ export const ExploreForm = ({ handleSearch }) => {
                 <label htmlFor="searchOptions" className="mb-2 text-3xl font-['Lobster_Two']">Search by:</label>
                 </div>
                 <div className="mb-6 grid md:grid-cols-2 gap-2">
-                    <select defaultValue="" required id="searchOptions" name="searchOptions" className="text-lg font-['Cutive_Mono'] shadow shadow-pink-500/50 p-2 border border-pink-500 outline-0 rounded caret-pink-500 bg-zinc-700">
+                    <select onChange={(e) => changePlaceHolder(e)} defaultValue="" required id="searchOptions" name="searchOptions" className="text-lg font-['Cutive_Mono'] shadow shadow-pink-500/50 p-2 border border-pink-500 outline-0 rounded caret-pink-500 bg-zinc-700">
                         <option disabled value="">Choose an option</option>
                         <option value="name">Country</option>
                         <option value="capital">Capital city</option>
                         <option value="region">Region</option>
                         <option value="subregion">Subregion</option>
                     </select>
-                    <input required type="search" placeholder="e.g. France" name="searchQuery" className="text-lg font-['Cutive_Mono'] shadow shadow-pink-500/50 p-2 border border-pink-500 outline-0 rounded caret-pink-500 bg-zinc-700"></input>
+                    <input ref={inputRef} required type="search" placeholder="" name="searchQuery" className="text-lg font-['Cutive_Mono'] shadow shadow-pink-500/50 p-2 border border-pink-500 outline-0 rounded caret-pink-500 bg-zinc-700"></input>
                 </div>
                 <div className="mb-6 grid">
                     <button type="submit" className="font-['Cutive_Mono'] text-xl bg-pink-500 p-1 md:p-3 mx-2 shadow-lg shadow-pink-500/50 hover:bg-pink-600 hover:shadow-pink-600/50 transition-colors rounded md:w-min w-full mx-auto">Find</button>
